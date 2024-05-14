7f2b058d43101f8d3b536045f0734889c80de11d
eberss123 <eber.lopez@uabc.edu.mx>
2024-05-14 12:37:45 -0700

    Tarea de clase Meta 3.3

---
+++ src/Camion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ebers
+ */
+public class Camion implements Vehiculo, Vehiculodecarga {
+
+    @Override
+    public void acelerar() {
+        System.out.println("el camion esta acelerando"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void frenar() {
+        System.out.println("el camion esta frenando"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void Cargarmercancia() {
+        System.out.println("el camion esta cargando mercancia"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Camion camion = new Camion();
+        camion.acelerar();
+        camion.frenar();
+        camion.Cargarmercancia();
+    }
+}
+
